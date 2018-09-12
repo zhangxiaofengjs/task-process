b779a66db83c9590a5bd05b0d1181f7fd2162ff0
zhangxiaofengjs <zhangxiaofengjs@163.com>
2018-09-12 11:16:56 +0900

    gui

---
+++ src/tpgui/FormTaskEdit.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xworks.taskprocess
+{
+    public partial class FormTaskEdit : Form
+    {
+        ListViewItem oldItem = new ListViewItem();
+		public FormTaskEdit()
+		{
+			InitializeComponent();
+		}
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ src/tpgui/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xworks.taskprocess
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+			try
+			{
+				DirectoryInfo di = new DirectoryInfo(@"D:\#tmp\0627");  
+				DirectorySecurity s = new DirectorySecurity(@"D:\#tmp\0627", AccessControlSections.Access);  
+       
+				//判断目录是否 可以访问  
+				if (!s.AreAccessRulesProtected)
+				{
+				}
+				FileInfo file = new FileInfo(@"D:\#tmp\0627");
+				AuthorizationRuleCollection accessRules = file.GetAccessControl().GetAccessRules(true, true,
+										typeof(System.Security.Principal.SecurityIdentifier));
+				foreach (FileSystemAccessRule rule in accessRules)
+				{
+					Debug.Write(rule.IdentityReference.Translate(typeof(NTAccount)) + "   ");
+					Debug.Write(rule.FileSystemRights);
+					Debug.WriteLine("  " + rule.AccessControlType);
+				}
+			}
+			catch (UnauthorizedAccessException)
+			{
+				MessageBox.Show("aaa");
+			}
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormTaskList());
+        }
+       
+    }
+}
